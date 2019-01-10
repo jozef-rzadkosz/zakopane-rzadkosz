9368778f105ba19d522431385bfe147119367ae2
freestyle09 <jozefrzadkosz09@gmail.com>
2019-01-10 15:31:42 +0100

    finished startup configuration

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import '../styles/style.scss';
+
 class App extends Component {
   state = {};
   render() {
-    return <div>App Component!</div>;
+    return (
+      <div className='container'>
+        {' '}
+        <i className='fa fa-heart' /> App Component! :)
+      </div>
+    );
   }
 }
 
