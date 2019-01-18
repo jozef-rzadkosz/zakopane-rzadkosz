56b1dc770aa4bd028e9f3ea3ce6943dc08e4c509
freestyle09 <jozefrzadkosz09@gmail.com>
2019-01-18 14:41:07 +0100

    set grid to location

--- client/src/components/Location.jsx
+++ client/src/components/Location.jsx
@@ -9,6 +9,19 @@ class Location extends Component {
           <div>
             <h2>Lokalizacja</h2>
             <h3>Mapka i przydatne informacje</h3>
+            <div className='location-info'>
+              <div>
+                <div>
+                  <div className='location-header'>5 km</div>
+                  <h4>Centrum Zakopane Krupówki</h4>
+                </div>
+                <div>3</div>
+              </div>
+              <div>
+                <div>2</div>
+                <div>4</div>
+              </div>
+            </div>
           </div>
           <div>2</div>
         </div>
