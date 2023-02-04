e71840a6a595bdd37a06b068bf5cac08ee95e275
jozefrzadkosz <jozefrzadkosz09@gmail.com>
2023-02-04 18:41:00 +0100

    Fixed links and added og image

--- mixins/navItems.js
+++ mixins/navItems.js
@@ -12,8 +12,17 @@ export default {
         { id: 4, name: 'Kontakt', url: 'kontakt' },
       ],
       socialItems: [
-        { id: 1, name: 'facebook-f', url: 'https://facebook.com' },
-        { id: 2, name: 'google', url: 'https://google.pl' },
+        // { id: 1, name: 'facebook-f', url: 'https://facebook.com' },
+        {
+          id: 2,
+          name: 'google',
+          url: 'https://www.google.com/search?q=zakopane-rzadkosz.pl&sxsrf=AJOqlzX-k4Bp4SyXtDLTNkxLqUjT0XQcPg%3A1675531936111&ei=oJbeY8O5BoznrgSnjL3QCA&ved=0ahUKEwjDifLusvz8AhWMs4sKHSdGD4oQ4dUDCA8&uact=5&oq=zakopane-rzadkosz.pl&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCC4QgAQQxwEQrwE6CggAEEcQ1gQQsAM6BggAEAgQHkoECEEYAEoECEYYAFDgBFjWBmDfB2gBcAF4AIABTIgB4AGSAQEzmAEAoAEByAEIwAEB&sclient=gws-wiz-serp',
+        },
+        {
+          id: 3,
+          name: 'earth',
+          url: 'https://www.olx.pl/d/oferta/noclegi-zakopane-pokoje-goscinne-krystyna-rzadkosz-CID-IDSLQWr.html?fbclid=IwAR3CVA9LxKkEdJ36IrJOx0w5tU_L5XNxLt4sz4JWcdnMj_AutDGFQQUBxKs',
+        },
       ],
     };
   },
